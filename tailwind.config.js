061a9e99b84530dc88887be7071c686bdd1df125
kieron <kieronboz@gmail.com>
2021-02-20 11:09:18 +0000

    deploy new tailwind settings

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+  theme: {
+    extend: {
+      zIndex: {
+        "-10": "-10",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
