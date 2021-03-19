0d925be7252382d15c4f5478f9a5a5b1557efee9
kieron <kieronboz@gmail.com>
2021-03-19 22:25:26 +0000

    fix footer background dark mode

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template functional>
   <div
-    class="items-center justify-between w-full px-8 py-6 text-gray-700 border-t dark:border-gray-600 lg:flex"
+    class="items-center justify-between w-full px-8 py-6 text-gray-700 border-t dark:bg-gray-800 dark:border-gray-600 lg:flex"
   >
     <p class="mb-2 lg:mb-0 dark:text-gray-400">©RUBRIC Copyright 2021</p>
 
