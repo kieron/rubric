de187026e743bfb9b36951526410be2124ebb558
kieron <kieronboz@gmail.com>
2022-03-21 11:01:24 +0000

    remove free message

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -4,7 +4,7 @@
 
     <div class="items-center mb-6">
       <h1
-        class="mb-3 text-3xl font-semibold text-gray-800 dark:text-gray-300 lg:mb-0"
+        class="mb-3 text-3xl font-semibold text-gray-800  dark:text-gray-300 lg:mb-0"
       >
         My Account
       </h1>
@@ -30,19 +30,28 @@
             :pk="publishableKey"
             :session-id="sessionId"
           />
+          <ErrorMessage
+            class="flex flex-row items-center p-5 mt-5 bg-red-200 border-b-2 border-red-300 rounded  alert swing-in-top-fwd"
+            v-if="errorHandler.error && loader.loading != true"
+            v-bind:message="errorHandler.errorMessage"
+          />
+          <SuccessMessage
+            class="flex flex-row items-center p-5 mt-5 bg-green-200 border-b-2 border-green-300 rounded  alert swing-in-top-fwd"
+            v-if="successMessage && !errorHandler.error && activeSub !== false"
+            v-bind:message="successMessage"
+          />
           <div class="container px-8 mx-auto">
             <h3
-              class="text-xl font-semibold text-center text-gray-700 my-7 dark:text-gray-300"
+              class="text-xl font-semibold text-center text-gray-700  my-7 dark:text-gray-300"
             >
               Pick a plan to get started, and pay nothing for 10 days!
             </h3>
             <div
-              class="flex flex-wrap text-center lg:divide-x dark:divide-gray-600"
-              title="Thanks for testing! Right now, Rubric SEO is FREE!"
+              class="flex flex-wrap text-center  lg:divide-x dark:divide-gray-600"
             >
               <div class="order-3 w-full px-8 py-6 mx-auto lg:order-1 lg:w-1/3">
                 <h2
-                  class="mb-3 text-lg font-semibold text-gray-700 dark:text-gray-300 lg:text-2xl title-font"
+                  class="mb-3 text-lg font-semibold text-gray-700  dark:text-gray-300 lg:text-2xl title-font"
                 >
                   Just Start
                 </h2>
@@ -50,10 +59,10 @@
                   Generate up to 50 reports a month.
                 </p>
                 <div
-                  class="flex flex-col items-center justify-center px-2 text-center lg:h-32"
+                  class="flex flex-col items-center justify-center px-2 text-center  lg:h-32"
                 >
                   <h2
-                    class="flex items-center justify-center mt-2 mb-4 text-3xl font-bold leading-none text-black dark:text-gray-300 lg:text-6xl"
+                    class="flex items-center justify-center mt-2 mb-4 text-3xl font-bold leading-none text-black  dark:text-gray-300 lg:text-6xl"
                   >
                     $8.99
                     <span class="ml-1 text-base text-gray-600">/mo</span>
@@ -62,14 +71,14 @@
                 <button
                   v-if="sessionId === ''"
                   @click="pay(justStartPlan)"
-                  class="flex items-center px-8 py-3 mx-auto mt-3 font-semibold text-white duration-150 bg-indigo-600 rounded-lg sm:w-auto hover:bg-indigo-500 focus:outline-none whitespace-nowrap"
+                  class="flex items-center px-8 py-3 mx-auto mt-3 font-semibold text-white duration-150 bg-indigo-600 rounded-lg  sm:w-auto hover:bg-indigo-500 focus:outline-none whitespace-nowrap"
                 >
                   Subscribe
                 </button>
               </div>
               <div class="order-2 w-full px-8 py-6 mx-auto lg:order-1 lg:w-1/3">
                 <h2
-                  class="mb-3 text-lg font-semibold text-gray-700 dark:text-gray-300 lg:text-2xl title-font"
+                  class="mb-3 text-lg font-semibold text-gray-700  dark:text-gray-300 lg:text-2xl title-font"
                 >
                   Pro
                 </h2>
@@ -77,10 +86,10 @@
                   Generate up to 100 reports a month.
                 </p>
                 <div
-                  class="flex flex-col items-center justify-center px-2 text-center lg:h-32"
+                  class="flex flex-col items-center justify-center px-2 text-center  lg:h-32"
                 >
                   <h2
-                    class="flex items-center justify-center mt-2 mb-4 text-3xl font-bold leading-none text-black dark:text-gray-300 lg:text-6xl"
+                    class="flex items-center justify-center mt-2 mb-4 text-3xl font-bold leading-none text-black  dark:text-gray-300 lg:text-6xl"
                   >
                     $19.99
                     <span class="ml-1 text-base text-gray-600">/mo</span>
@@ -89,14 +98,14 @@
                 <button
                   v-if="sessionId === ''"
                   @click="pay(proPlan)"
-                  class="flex items-center px-8 py-3 mx-auto mt-3 font-semibold text-white duration-150 bg-indigo-600 rounded-lg sm:w-auto hover:bg-indigo-500 focus:outline-none whitespace-nowrap"
+                  class="flex items-center px-8 py-3 mx-auto mt-3 font-semibold text-white duration-150 bg-indigo-600 rounded-lg  sm:w-auto hover:bg-indigo-500 focus:outline-none whitespace-nowrap"
                 >
                   Subscribe
                 </button>
               </div>
               <div class="order-1 w-full px-8 py-6 mx-auto lg:order-1 lg:w-1/3">
                 <h2
-                  class="mb-3 text-lg font-semibold text-gray-700 dark:text-gray-300 lg:text-2xl title-font"
+                  class="mb-3 text-lg font-semibold text-gray-700  dark:text-gray-300 lg:text-2xl title-font"
                 >
                   Agency
                 </h2>
@@ -104,10 +113,10 @@
                   Generate 500 reports a month!
                 </p>
                 <div
-                  class="flex flex-col items-center justify-center px-2 text-center lg:h-32"
+                  class="flex flex-col items-center justify-center px-2 text-center  lg:h-32"
                 >
                   <h2
-                    class="flex items-center justify-center mt-2 mb-4 text-3xl font-bold leading-none text-black dark:text-gray-300 lg:text-6xl"
+                    class="flex items-center justify-center mt-2 mb-4 text-3xl font-bold leading-none text-black  dark:text-gray-300 lg:text-6xl"
                   >
                     $34.99
                     <span class="ml-1 text-base text-gray-600">/mo</span>
@@ -116,7 +125,7 @@
                 <button
                   v-if="sessionId === ''"
                   @click="pay(agencyPlan)"
-                  class="flex items-center px-8 py-3 mx-auto mt-3 font-semibold text-white duration-150 bg-indigo-600 rounded-lg sm:w-auto hover:bg-indigo-500 focus:outline-none whitespace-nowrap"
+                  class="flex items-center px-8 py-3 mx-auto mt-3 font-semibold text-white duration-150 bg-indigo-600 rounded-lg  sm:w-auto hover:bg-indigo-500 focus:outline-none whitespace-nowrap"
                 >
                   Subscribe
                 </button>
@@ -199,7 +208,7 @@
             <form @submit.prevent="portal" class="w-full">
               <button
                 type="submit"
-                class="items-center self-center w-full h-16 px-4 mb-2 font-semibold text-white duration-150 bg-indigo-600 rounded-lg sm:w-auto hover:bg-indigo-500 focus:outline-none whitespace-nowrap"
+                class="items-center self-center w-full h-16 px-4 mb-2 font-semibold text-white duration-150 bg-indigo-600 rounded-lg  sm:w-auto hover:bg-indigo-500 focus:outline-none whitespace-nowrap"
               >
                 Open Billing Portal
               </button>
@@ -209,7 +218,7 @@
 
         <div class="mt-5" v-if="userSerps.length">
           <h2
-            class="mb-3 text-2xl font-semibold text-gray-800 dark:text-gray-300 lg:mb-0"
+            class="mb-3 text-2xl font-semibold text-gray-800  dark:text-gray-300 lg:mb-0"
           >
             Your Reports
           </h2>
@@ -219,32 +228,32 @@
           </p>
           <hr class="mt-2 dark:border-gray-600" />
           <div
-            class="flex flex-col mt-5 mb-12 border border-gray-300 rounded-md dark:border-gray-600 bg-gray-25 dark:bg-gray-700"
+            class="flex flex-col mt-5 mb-12 border border-gray-300 rounded-md  dark:border-gray-600 bg-gray-25 dark:bg-gray-700"
           >
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div
-                class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8"
+                class="inline-block min-w-full py-2 align-middle  sm:px-6 lg:px-8"
               >
                 <div class="overflow-hidden sm:rounded-lg">
                   <table
-                    class="min-w-full divide-y divide-gray-200 dark:divide-gray-600"
+                    class="min-w-full divide-y divide-gray-200  dark:divide-gray-600"
                   >
                     <thead>
                       <tr>
                         <th
-                          class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-700 uppercase dark:text-gray-300 dark:bg-gray-700 bg-gray-50"
+                          class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-700 uppercase  dark:text-gray-300 dark:bg-gray-700 bg-gray-50"
                         >
                           Report
                         </th>
                         <td
-                          class="py-3 text-xs font-medium leading-4 tracking-wider text-center text-gray-700 uppercase dark:text-gray-300 dark:bg-gray-700 bg-gray-50"
+                          class="py-3 text-xs font-medium leading-4 tracking-wider text-center text-gray-700 uppercase  dark:text-gray-300 dark:bg-gray-700 bg-gray-50"
                         >
                           Analysis
                         </td>
                       </tr>
                     </thead>
                     <tbody
-                      class="bg-white border-t border-gray-300 divide-y divide-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:divide-gray-600"
+                      class="bg-white border-t border-gray-300 divide-y divide-gray-200  dark:border-gray-600 dark:bg-gray-800 dark:divide-gray-600"
                     >
                       <tr
                         :key="index"
@@ -255,13 +264,13 @@
                           <div class="flex items-center">
                             <div class="">
                               <div
-                                class="mb-1 mr-16 font-medium leading-5 text-gray-900 dark:text-gray-300 md:text-lg"
+                                class="mb-1 mr-16 font-medium leading-5 text-gray-900  dark:text-gray-300 md:text-lg"
                               >
                                 {{ report.searchTerm }}
                               </div>
 
                               <div
-                                class="text-xs leading-5 text-gray-500 dark:text-gray-400 md:text-sm"
+                                class="text-xs leading-5 text-gray-500  dark:text-gray-400 md:text-sm"
                               >
                                 <span class="break-all">{{
                                   convertTime(report.createdAt)
@@ -275,13 +284,13 @@
                             <router-link
                               :to="`/analysis?retrieve=${report._id}`"
                               tag="button"
-                              class="w-auto h-10 px-4 mx-auto mb-1 font-semibold text-white duration-150 bg-indigo-600 rounded-lg hover:bg-indigo-500 focus:outline-none whitespace-nowrap"
+                              class="w-auto h-10 px-4 mx-auto mb-1 font-semibold text-white duration-150 bg-indigo-600 rounded-lg  hover:bg-indigo-500 focus:outline-none whitespace-nowrap"
                               title="Open Report"
                             >
                               <ArrowCircleRightIcon class="flex align-middle" />
                             </router-link>
                             <button
-                              class="w-auto h-10 px-4 mx-auto font-semibold text-white duration-150 bg-red-600 rounded-lg md:mt-0 focus:outline-none whitespace-nowrap"
+                              class="w-auto h-10 px-4 mx-auto font-semibold text-white duration-150 bg-red-600 rounded-lg  md:mt-0 focus:outline-none whitespace-nowrap"
                               @click="deleteReport(index, report)"
                               title="Delete Report"
                             >
@@ -306,7 +315,7 @@
         </div>
         <div class="mt-5" v-else>
           <h2
-            class="mb-3 text-2xl font-semibold text-gray-800 dark:text-gray-300 lg:mb-0"
+            class="mb-3 text-2xl font-semibold text-gray-800  dark:text-gray-300 lg:mb-0"
           >
             Your Reports
           </h2>
@@ -323,7 +332,7 @@
             </p>
             <div class="flex justify-center mt-6 lg:justify-start">
               <router-link
-                class="flex items-center self-center h-16 px-4 mb-2 font-semibold text-white duration-150 bg-indigo-600 rounded-lg hover:bg-indigo-500 focus:outline-none"
+                class="flex items-center self-center h-16 px-4 mb-2 font-semibold text-white duration-150 bg-indigo-600 rounded-lg  hover:bg-indigo-500 focus:outline-none"
                 to="/analysis"
                 v-if="isLoggedIn"
                 >SERP Analysis</router-link
@@ -333,7 +342,7 @@
         </div>
         <div class="mt-5" v-if="userBlueprints.length">
           <h2
-            class="mb-3 text-2xl font-semibold text-gray-800 dark:text-gray-300 lg:mb-0"
+            class="mb-3 text-2xl font-semibold text-gray-800  dark:text-gray-300 lg:mb-0"
           >
             Your Blueprints
           </h2>
@@ -343,32 +352,32 @@
           </p>
           <hr class="mt-2 dark:border-gray-600" />
           <div
-            class="flex flex-col mt-5 mb-12 border border-gray-300 rounded-md dark:border-gray-600 bg-gray-25 dark:bg-gray-700"
+            class="flex flex-col mt-5 mb-12 border border-gray-300 rounded-md  dark:border-gray-600 bg-gray-25 dark:bg-gray-700"
           >
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div
-                class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8"
+                class="inline-block min-w-full py-2 align-middle  sm:px-6 lg:px-8"
               >
                 <div class="overflow-hidden sm:rounded-lg">
                   <table
-                    class="min-w-full divide-y divide-gray-200 dark:divide-gray-600"
+                    class="min-w-full divide-y divide-gray-200  dark:divide-gray-600"
                   >
                     <thead>
                       <tr>
                         <th
-                          class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-700 uppercase dark:text-gray-300 dark:bg-gray-700 bg-gray-50"
+                          class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-700 uppercase  dark:text-gray-300 dark:bg-gray-700 bg-gray-50"
                         >
                           Blueprint
                         </th>
                         <td
-                          class="py-3 text-xs font-medium leading-4 tracking-wider text-center text-gray-700 uppercase dark:text-gray-300 dark:bg-gray-700 bg-gray-50"
+                          class="py-3 text-xs font-medium leading-4 tracking-wider text-center text-gray-700 uppercase  dark:text-gray-300 dark:bg-gray-700 bg-gray-50"
                         >
                           Options
                         </td>
                       </tr>
                     </thead>
                     <tbody
-                      class="bg-white border-t border-gray-300 divide-y divide-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:divide-gray-600"
+                      class="bg-white border-t border-gray-300 divide-y divide-gray-200  dark:border-gray-600 dark:bg-gray-800 dark:divide-gray-600"
                     >
                       <tr
                         :key="index"
@@ -379,13 +388,13 @@
                           <div class="flex items-center">
                             <div class="">
                               <div
-                                class="mb-1 mr-16 font-medium leading-5 text-gray-900 dark:text-gray-300 md:text-lg"
+                                class="mb-1 mr-16 font-medium leading-5 text-gray-900  dark:text-gray-300 md:text-lg"
                               >
                                 {{ blueprint.searchTerm }}
                               </div>
 
                               <div
-                                class="text-xs leading-5 text-gray-500 dark:text-gray-400 md:text-sm"
+                                class="text-xs leading-5 text-gray-500  dark:text-gray-400 md:text-sm"
                               >
                                 <span class="break-all">{{
                                   convertTime(blueprint.createdAt)
@@ -399,13 +408,13 @@
                             <router-link
                               :to="`/blueprint?retrieve=${blueprint._id}`"
                               tag="button"
-                              class="w-auto h-10 px-4 mx-auto mb-1 font-semibold text-white duration-150 bg-indigo-600 rounded-lg hover:bg-indigo-500 focus:outline-none whitespace-nowrap"
+                              class="w-auto h-10 px-4 mx-auto mb-1 font-semibold text-white duration-150 bg-indigo-600 rounded-lg  hover:bg-indigo-500 focus:outline-none whitespace-nowrap"
                               title="Open Blueprint"
                             >
                               <ArrowCircleRightIcon class="flex align-middle" />
                             </router-link>
                             <button
-                              class="w-auto h-10 px-4 mx-auto font-semibold text-white duration-150 bg-red-600 rounded-lg md:mt-0 focus:outline-none whitespace-nowrap"
+                              class="w-auto h-10 px-4 mx-auto font-semibold text-white duration-150 bg-red-600 rounded-lg  md:mt-0 focus:outline-none whitespace-nowrap"
                               @click="deleteBlueprint(index, blueprint)"
                               title="Delete Report"
                             >
@@ -429,16 +438,6 @@
           </div>
         </div>
       </div>
-      <ErrorMessage
-        class="flex flex-row items-center p-5 mt-5 bg-red-200 border-b-2 border-red-300 rounded alert swing-in-top-fwd"
-        v-if="errorHandler.error && loader.loading != true"
-        v-bind:message="errorHandler.errorMessage"
-      />
-      <SuccessMessage
-        class="flex flex-row items-center p-5 mt-5 bg-green-200 border-b-2 border-green-300 rounded alert swing-in-top-fwd"
-        v-if="successMessage && !errorHandler.error && activeSub !== false"
-        v-bind:message="successMessage"
-      />
     </div>
   </div>
 </template>
